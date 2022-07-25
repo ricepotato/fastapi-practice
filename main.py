96af6a7dfd66a39835dcff3f9b2b927c1d5a2dda
ricepotato <sukjun40@naver.com>
2022-07-26 03:13:55 +0900

    path parameters, query parameters

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Union
 from enum import Enum
 from fastapi import FastAPI
 
@@ -16,13 +17,48 @@ async def root():
 
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+async def read_item(item_id: int, q: Union[str, None] = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+
+@app.get("/shots/{shot_id}")
+async def read_user_item(shot_id: str, needy: str):
+    item = {"shot_id": shot_id, "needy": needy}
+    return item
+
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items")
+async def read_item_with_params(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
 
 
 @app.get("/users/me")
 async def read_user_me():
     return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: Union[str, None] = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
 
 
 @app.get("/users/{user_id}")
